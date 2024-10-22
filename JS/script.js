9b0e81d6b0937e031d5c9ef17f1e07a90ce8ee9e
PedroDelgo <pedrodelg.go@gmail.com>
2024-10-22 18:58:25 -0300

    chore: add comments to the code

--- JS/script.js
+++ JS/script.js
@@ -3,9 +3,17 @@ const dialogRegistrar = document.getElementById('dialog-registrar-ponto')
 const btnDialogFechar = document.getElementById('dialog-fechar')
 
 
+////////////////////////////////
+// Função para abrir o Dialog //
+////////////////////////////////
+
 btnBaterPonto.addEventListener('click', ()=>{
     dialogRegistrar.showModal()
 })
+
+/////////////////////////////////
+// Função para fechar o Dialog //
+/////////////////////////////////
 
 btnDialogFechar.addEventListener('click', ()=>{
     dialogRegistrar.close()
