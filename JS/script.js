42a13760d10d278abcb76f5f05906ebbf6d991a2
Bruno César <bcgmeireles@gmail.com>
2024-10-23 20:32:46 -0300

    feat: implements dialog component and blur background

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,15 @@
-const btnBaterPonto = document.getElementById('btn-bater-ponto')
-const dialogRegistrar = document.getElementById('dialog-registrar-ponto')
-const btnDialogFechar = document.getElementById('dialog-fechar')
+const btnBaterPonto = document.getElementById('btn-bater-ponto');
+const dialogRegistrar = document.getElementById('dialog-registrar-ponto');
+const fecharDialogBaterPonto = document.getElementById('fechar-dialog-bater-ponto');
+const dialogBaterPonto = document.getElementById('dialog-bater-ponto');
+const blurBackground = document.getElementById('blur-background');
+
+btnBaterPonto.addEventListener('click', () => {
+    dialogBaterPonto.style.display = 'flex';
+    blurBackground.style.display = 'block';
+});
+
+fecharDialogBaterPonto.addEventListener('click', () => {
+    dialogBaterPonto.style.display = 'none';
+    blurBackground.style.display = 'none';
+});
