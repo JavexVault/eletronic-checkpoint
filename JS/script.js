2cd04ba3e9ce8b100675ef00f3908d21f5b48e9c
PedroDelgo <pedrodelg.go@gmail.com>
2024-10-23 19:18:42 -0300

    feat: Creates 'dialog-bater-ponto'

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,3 @@
 const btnBaterPonto = document.getElementById('btn-bater-ponto')
 const dialogRegistrar = document.getElementById('dialog-registrar-ponto')
 const btnDialogFechar = document.getElementById('dialog-fechar')
-
-
-////////////////////////////////
-// Função para abrir o Dialog //
-////////////////////////////////
-
-btnBaterPonto.addEventListener('click', ()=>{
-    dialogRegistrar.showModal()
-})
-
-/////////////////////////////////
-// Função para fechar o Dialog //
-/////////////////////////////////
-
-btnDialogFechar.addEventListener('click', ()=>{
-    dialogRegistrar.close()
-})
