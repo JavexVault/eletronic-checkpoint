566028d32a74c37a861b87755537655a773bf2d3
PedroDelgo <pedrodelg.go@gmail.com>
2024-10-23 21:13:55 -0300

    feat: implements getDate function

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,9 @@ const dialogRegistrar = document.getElementById('dialog-registrar-ponto');
 const fecharDialogBaterPonto = document.getElementById('fechar-dialog-bater-ponto');
 const dialogBaterPonto = document.getElementById('dialog-bater-ponto');
 const blurBackground = document.getElementById('blur-background');
-
 const hour = document.getElementById('hour');
+
+const data = document.getElementById('date');
 
 btnBaterPonto.addEventListener('click', () => {
     dialogBaterPonto.style.display = 'flex';
@@ -21,9 +22,17 @@ function getHour() {
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
-
     hour.innerText = `${hours}:${minutes}:${seconds}`;
 }
-
 setInterval(getHour,0000)
 
+function getDate (){
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2,"0");
+    const month = String(date.getMonth() + 1).padStart(2,"0");
+    const year = date.getFullYear();
+
+    data.innerText = `${day}/${month}/${year}`
+
+}
+getDate()
